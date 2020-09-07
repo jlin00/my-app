55d49fc3855155a20673eda8dc7db0e6a00aaaa5
Jackie Lin <jackielin655@gmail.com>
2020-09-07 15:53:38 -0400

    removed test page

--- src/App.js
+++ src/App.js
@@ -3,14 +3,12 @@ import { Route } from 'react-router-dom';
 
 import Home from './home';
 import UserInfo from './UserInfo';
-import Project from './project';
 
 function App() {
   return (
     <div>
       <Route exact path="/" component={Home} />
       <Route exact path="/users" component={UserInfo} />
-      <Route path="/project" component={Project} />
     </div>
   );
 }
