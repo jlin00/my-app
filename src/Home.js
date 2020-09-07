402148bded62eb1ff209b9a2d9f8f75319160986
Jackie Lin <jackielin655@gmail.com>
2020-09-07 15:47:33 -0400

    added home form

--- src/Home.js
+++ src/Home.js
@@ -4,7 +4,14 @@ import './App.css';
 function Home() {
   return(
     <div>
-      <h2>Home Component</h2>
+      <h2>Login</h2>
+      <form action="/users">
+        <label for="username">Username:</label><br />
+        <input type="text" id="username" name="username" /><br />
+        <label for="password">Password:</label><br />
+        <input type="password" id="password" name="password" /><br /><br />
+        <input type="submit" value="Submit" />
+      </form>
     </div>
   );
 }
